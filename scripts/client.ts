958986c405c6119c9006f205c0bcbc0028389e26
Viktor Valentinov <vvalntnv@gmail.com>
2025-12-03 11:16:57 +0200

    feat: too much abstraction is bad

--- scripts/client.ts
+++ scripts/client.ts
@@ -1,55 +1,88 @@
 import {
-  type Rpc,
-  type RpcSubscriptions,
-  type SolanaRpcApi,
-  type SolanaRpcSubscriptionsApi,
-  type TransactionSigner,
-  type MessageSigner,
+  airdropFactory,
+  appendTransactionMessageInstruction,
+  BaseTransactionMessage,
   sendAndConfirmTransactionFactory,
+  generateKeyPairSigner,
+  lamports,
+  MessageSigner,
+  Rpc,
+  RpcSubscriptions,
+  SolanaRpcApi,
+  SolanaRpcSubscriptionsApi,
+  TransactionMessageWithFeePayer,
+  TransactionSigner,
 } from "@solana/kit";
-import { createConnections } from "./rpc/connections";
-import { createAndFundWallet } from "./wallet/createWallet";
-import { createComputeUnitEstimator } from "./transactions/computeUnits";
-import { createTransactionSender } from "./transactions/sender";
 
-/**
- * The client holds all the tools needed to interact with Solana
- */
+import { createSolanaRpc, createSolanaRpcSubscriptions } from "@solana/kit";
+import {
+  estimateComputeUnitLimitFactory,
+  getSetComputeUnitLimitInstruction,
+} from "@solana-program/compute-budget";
+
+function estimateAndSetComputeUnitLimitFactory(
+  ...params: Parameters<typeof estimateComputeUnitLimitFactory>
+) {
+  const estimateComputeUnitLimit = estimateComputeUnitLimitFactory(...params);
+  return async <
+    T extends BaseTransactionMessage & TransactionMessageWithFeePayer,
+  >(
+    transactionMessage: T,
+  ) => {
+    const computeUnitsEstimate =
+      await estimateComputeUnitLimit(transactionMessage);
+    return appendTransactionMessageInstruction(
+      getSetComputeUnitLimitInstruction({ units: computeUnitsEstimate }),
+      transactionMessage,
+    );
+  };
+}
+
 export type Client = {
-  estimateAndSetComputeUnitLimit: ReturnType<typeof createComputeUnitEstimator>;
-  sendAndConfirmTransaction: ReturnType<typeof createTransactionSender>;
+  estimateAndSetComputeUnitLimit: ReturnType<
+    typeof estimateAndSetComputeUnitLimitFactory
+  >;
+  sendAndConfirmTransaction: ReturnType<
+    typeof sendAndConfirmTransactionFactory
+  >;
   rpc: Rpc<SolanaRpcApi>;
   rpcSubscriptions: RpcSubscriptions<SolanaRpcSubscriptionsApi>;
   wallet: TransactionSigner & MessageSigner;
 };
 
 let client: Client | undefined;
-
-/**
- * Creates a Solana client with wallet, RPC connections, and utilities
- */
 export async function createClient(): Promise<Client> {
-  if (client) {
-    return client;
+  if (!client) {
+    const signer = await generateKeyPairSigner();
+    const rpc = createSolanaRpc("http://127.0.0.1:8899");
+    const rpcSubscriptions = createSolanaRpcSubscriptions(
+      "ws://127.0.0.1:8900",
+    );
+
+    const airdrop = airdropFactory({ rpc, rpcSubscriptions });
+    const sendAndConfirmTransaction = sendAndConfirmTransactionFactory({
+      rpc,
+      rpcSubscriptions,
+    });
+
+    await airdrop({
+      recipientAddress: signer.address,
+      lamports: lamports(100_000_000_000n),
+      commitment: "confirmed",
+    });
+
+    const estimateAndSetComputeUnitLimit =
+      estimateAndSetComputeUnitLimitFactory({
+        rpc,
+      });
+
+    client = {
+      estimateAndSetComputeUnitLimit,
+      sendAndConfirmTransaction,
+      rpc,
+      rpcSubscriptions,
+      wallet: signer,
+    };
   }
-
-  const { rpc, rpcSubscriptions } = createConnections();
-
-  const wallet = await createAndFundWallet(rpc, rpcSubscriptions);
-
-  const estimateAndSetComputeUnitLimit = createComputeUnitEstimator(rpc);
-  const sendAndConfirmTransaction = sendAndConfirmTransactionFactory({
-    rpc,
-    rpcSubscriptions,
-  });
-
-  client = {
-    estimateAndSetComputeUnitLimit,
-    sendAndConfirmTransaction,
-    rpc,
-    rpcSubscriptions,
-    wallet,
-  };
-
   return client;
 }
--- scripts/createToken.ts
+++ scripts/createToken.ts
@@ -1,16 +1,93 @@
+import * as solana from "@solana/kit";
 import { createClient } from "./client";
-import { createMint } from "./mint/createMint";
+import type { Client } from "./client";
+import { getInitializeMintInstruction } from "@solana-program/token";
+import { extension, getMintSize } from "@solana-program/token-2022";
+import { getCreateAccountInstruction } from "@solana-program/system";
+import {
+  getInitializeTransferFeeConfigInstruction,
+  TOKEN_2022_PROGRAM_ADDRESS,
+} from "@solana-program/token-2022";
 
-/**
- * Main script: Creates a Solana token (mint) with transfer fees
- */
 async function main() {
   const client = await createClient();
 
-  const mintAddress = await createMint(client);
+  await createMint(client);
+}
 
-  console.log("Token created successfully!");
-  console.log("Mint address:", mintAddress);
+async function createMint(client: Client) {
+  const mint = await solana.generateKeyPairSigner();
+
+  const transferFees = {
+    epoch: 0,
+    transferFeeBasisPoints: 50,
+    maximumFee: 50_000_000n,
+  };
+
+  const ext = extension("TransferFeeConfig", {
+    transferFeeConfigAuthority: client.wallet.address,
+    withdrawWithheldAuthority: client.wallet.address,
+    withheldAmount: 0n,
+    olderTransferFee: transferFees,
+    newerTransferFee: transferFees,
+  });
+
+  const mintSize = getMintSize([ext]);
+  const mintRent = await client.rpc
+    .getMinimumBalanceForRentExemption(BigInt(mintSize))
+    .send();
+
+  const createAccIx = getCreateAccountInstruction({
+    payer: client.wallet,
+    newAccount: mint,
+    space: mintSize,
+    lamports: mintRent,
+    programAddress: TOKEN_2022_PROGRAM_ADDRESS,
+  });
+
+  const initializeTransactionFeeIx = getInitializeTransferFeeConfigInstruction({
+    mint: mint.address,
+    withdrawWithheldAuthority: client.wallet.address,
+    transferFeeConfigAuthority: client.wallet.address,
+    transferFeeBasisPoints: ext.newerTransferFee.transferFeeBasisPoints,
+    maximumFee: ext.newerTransferFee.maximumFee,
+  });
+
+  const initializeMintIx = getInitializeMintInstruction({
+    mint: mint.address,
+    decimals: 6,
+    mintAuthority: client.wallet.address,
+    freezeAuthority: client.wallet.address,
+  });
+
+  const { value: latestBlockhash } = await client.rpc
+    .getLatestBlockhash()
+    .send();
+
+  const transactionMessage = await solana.pipe(
+    solana.createTransactionMessage({ version: 0 }),
+    (tx) => solana.setTransactionMessageFeePayerSigner(client.wallet, tx),
+    (tx) =>
+      solana.setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+    (tx) =>
+      solana.appendTransactionMessageInstructions(
+        [createAccIx, initializeTransactionFeeIx, initializeMintIx],
+        tx,
+      ),
+    (tx) => client.estimateAndSetComputeUnitLimit(tx),
+  );
+
+  const transaction =
+    await solana.signTransactionMessageWithSigners(transactionMessage);
+
+  solana.assertIsSendableTransaction(transaction);
+  solana.assertIsTransactionWithBlockhashLifetime(transaction);
+
+  await client.sendAndConfirmTransaction(transaction, {
+    commitment: "confirmed",
+  });
+
+  console.log("The mint address is: ", mint.address);
 }
 
 main();
